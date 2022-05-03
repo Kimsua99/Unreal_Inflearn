fd625c8540779d9505e73bc5c13d7b69acb5310f
sua <ksa3067@gmail.com>
2022-05-03 16:38:40 +0900

    델레게이트

--- Unreal_Engine/Source/TestUnrealEngine/MyCharacter.cpp
+++ Unreal_Engine/Source/TestUnrealEngine/MyCharacter.cpp
@@ -38,6 +38,8 @@ void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	AnimInstance = Cast<UMyAnimInstance>(GetMesh()->GetAnimInstance());
+	AnimInstance->OnMontageEnded.AddDynamic(this, &AMyCharacter::OnAttackMontageEnded);
 }
 
 // Called every frame
@@ -63,11 +65,12 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 void AMyCharacter::Attack()
 {
-	auto AnimInstance = Cast<UMyAnimInstance> (GetMesh()->GetAnimInstance());
-	if (AnimInstance)
-	{
-		AnimInstance->PlayAttackMontage();
-	}
+	if (isAttacking)
+		return;
+
+	AnimInstance->PlayAttackMontage();
+
+	isAttacking = true;
 }
 
 void AMyCharacter::UpDown(float Value)
@@ -91,4 +94,9 @@ void AMyCharacter::LeftRight(float Value)
 void AMyCharacter::Yaw(float Value)
 {
 	AddControllerYawInput(Value);
+}
+
+void AMyCharacter::OnAttackMontageEnded(UAnimMontage* Montage, bool binterruped)
+{
+	isAttacking = false;
 }
